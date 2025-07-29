ff4e1c4e88a2a218d78933f763cc5c69f8ec691b
ronak <rk8875636542@gmail.com>
2025-07-30 00:59:28 +0530

    first commit

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	// This is the main entry point of the application.
-	// You can add your application logic here.
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+			"data":    []any{1, "ronak", 3},
+		})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
